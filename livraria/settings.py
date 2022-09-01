1fc6ffacc7fb3d18acab415ebb2f18ebbd212ee1
gustavoanjosinfo <relogio.gustavo2000@gmail.com>
2022-09-01 16:23:02 -0300

    commit 0109

--- livraria/settings.py
+++ livraria/settings.py
@@ -113,5 +113,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
